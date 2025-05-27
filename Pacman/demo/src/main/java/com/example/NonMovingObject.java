91f85bf80b77272b8b0d8f8b2413a9faa61290f2
1sbeep <shobidhansivasothy@gmail.com>
2025-05-27 12:04:10 +0200

    Update NonMovingObject.java

--- Pacman/demo/src/main/java/com/example/NonMovingObject.java
+++ Pacman/demo/src/main/java/com/example/NonMovingObject.java
@@ -5,7 +5,7 @@ package com.example;
 import javafx.scene.image.Image;
 
 abstract class NonMovingObject {
-    public Image image;
+    public Image image; //skal den ikke være private, da vi har en getter til attributten?
 
     public Image getImage(){
         return this.image;
